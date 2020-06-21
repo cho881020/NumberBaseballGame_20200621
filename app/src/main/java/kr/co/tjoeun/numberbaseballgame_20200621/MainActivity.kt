75777c31e271a8a4d4c5a5f26e73f22fa0dd824a
cho881020 <kj_cho@idealidea.co.kr>
2020-06-21 14:01:18 +0900

    채팅 목록 화면 출현  => 실 데이터 미반영

--- app/src/main/java/kr/co/tjoeun/numberbaseballgame_20200621/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/numberbaseballgame_20200621/MainActivity.kt
@@ -2,8 +2,14 @@ package kr.co.tjoeun.numberbaseballgame_20200621
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.numberbaseballgame_20200621.adapters.ChatAdapter
+import kr.co.tjoeun.numberbaseballgame_20200621.datas.Chat
 
 class MainActivity : BaseActivity() {
+
+    val chatList = ArrayList<Chat>()
+    lateinit var mChatAdapter : ChatAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        chatList.add(Chat("CPU", "숫자 야구게임에 오신것을 환영합니다."))
+        chatList.add(Chat("CPU", "세자리 숫자를 맞춰주세요."))
+        chatList.add(Chat("CPU", "0은 포함되지 않으며, 중복된 숫자도 없습니다."))
+
+        mChatAdapter = ChatAdapter(mContext, R.layout.chat_list_item, chatList)
+        chatListView.adapter = mChatAdapter
 
     }
 
